b483723415fe55d2e7a2dfca73481b374d9285c1
Ryuichi1993 <huzimokunn0@gmail.com>
2020-01-08 09:19:24 +0000

    会員編集機能

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,2 +1,21 @@
 class Public::EndUsersController < ApplicationController
 end
+
+
+ def edit
+ 	 @end_user = EndUser.find(params[:id])
+ end
+
+
+
+
+
+
+
+
+
+
+
+ private
+ def end_user_params
+ 	params.require(:end_user).permit(:email, :name1, :name2, :name3, :name4, :address, :post, :number, :withdraw)
