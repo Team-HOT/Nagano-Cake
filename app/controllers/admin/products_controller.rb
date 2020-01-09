fc51a3f45269c8a8981182e8aeaa3f77098f5abe
Mai28 <Mai28>
2020-01-09 09:48:04 +0000

    テスト

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,1 +1,36 @@
 class Admin::ProductsController < ApplicationController
+def index
+     @products = Product.all
+
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+          product = Product.new(product_params)
+         product.save
+        redirect_to admin_products_index_path
+  end
+  
+
+
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def product_params
+        params.require(:product).permit(:name, :price, :product_image)
+    end
+end
