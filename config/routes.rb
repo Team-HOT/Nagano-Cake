30132613b0c21ec03756a18f8c1c3da7fd580126
Mai28 <Mai28>
2020-01-09 10:37:14 +0000

    Revert "ルーティングの編集"
    This reverts commit 137d05171d9cadcd2b7dfdb430119e829f70f2aa.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,39 @@
 Rails.application.routes.draw do
- namespace :admin do
-    resources :end_users, only:[:index, :show, :edit, :update]
+  namespace :admin do
+    get 'end_users/index'
+    get 'end_users/show'
+    get 'end_users/edit'
+    get 'end_users/update'
   end
   namespace :admin do
-    resources :product_categories, only:[:index, :create, :edit, :update]
+    get 'product_categories/index'
+    get 'product_categories/create'
+    get 'product_categories/edit'
+    get 'product_categories/update'
   end
   namespace :admin do
-    resources :products
+    get 'products/index'
+    get 'products/new'
+    post'products/create'
+    get 'products/show'
+    get 'products/edit'
+    patch 'products/update'
+    delete 'products/destroy'
   end
   namespace :admin do
-    resources :orders, only:[:index, :show, :update]
+    get 'home/top'
   end
-  namespace :admin do
-    get '/home', to:'home#top'
-  end
+  
   namespace :public do
     root :to => 'homes#top'
+
+    resources :products
   end
+
+
+
+
+
   devise_for :admins
   devise_for :end_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
