dfb5f929bd40059aaaf42a81b0e13a71c7f396e9
Mai28 <Mai28>
2020-01-13 11:43:47 +0000

    商品一覧と、商品詳細ページ追加

--- app/controllers/public/product_categories_controller.rb
+++ app/controllers/public/product_categories_controller.rb
@@ -1,35 +1,27 @@
 class Public::ProductCategoriesController < ApplicationController
-
-  def new
-  	@product_category = Product_category.new
-  end
-
-  def index
+ def index
     @product_category = ProductCategory.new
     @product_categories = ProductCategory.all
+ end
 
-  end
-  def create
-  	@product_category =  Product_category.new(product_params)
-    @product_category.save
-    redirect_to public_products_path
-  end
 
   def edit
   end
 
   def show
-    @product_category = Product_category.find(params[:id])
+   @product = Product.find(params[:id])
+   @product_categories = ProductCategory.all
+   @product_category = ProductCategory.find(params[:id])
+   # ケーキカテゴリーの商品
+   # @product_categoryの商品
+   @product_category.products
   end
 
   def update
   end
 
  private
- def product_category_params
-     params.require(:product_category).permit(:product_category_name)
-end
-
-
-
+  def product_category_params
+     params.require(:product_category).permit(:product_category_name,  :product_category_id)
+  end
 end
--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,31 +1,38 @@
 class Public::ProductsController < ApplicationController
+	# def index
+	#     @products = Product.where(product_category_id: 1)
+	#     @product = Product.new
+	#     @product_categories = ProductCategory.all
 
- def new
-    @product = Product.new
-  end
+	#   ends
 
-def index
-    @products = Product.all
-    @product = Product.new
-    @product_category = ProductCategory.all
-  end
+	def create
+	@product = Product.new(product_params)
+	@product.save
+	redirect_to admin_product_path(@product.id)
+	end
 
- def create
-    @product = Product.new(product_params)
-    @product.save
-    redirect_to admin_product_path(@product.id)
-  end
+	def category_show
+		@product = Product.find(params[:id])
+		@product_categories = ProductCategory.find(params[:id])
 
+	end
 
- def show
- 	@product = Product.find(params[:id])
- 	@product_new = Product.new
- end
+	def show
+		@product = Product.find(params[:id])
+		@product_new = Product.new
+		@product_categories = ProductCategory.all
+	end
 
 
 
-    private
-def product_params
-    params.require(:product).permit(:name, :price, :product_body, :product_image, :product_category_id, :sale_status)
-end
+
+
+	    private
+	 def product_params
+	    params.require(:product).permit(:name, :price, :product_body, :product_image, :product_category_id, :sale_status)
+	 end
+	 def product_category
+		params.require(:product_category).permit(:name)
+	 end
 end
